f2e01cb747eb3e62c7d567c3e1c658734ba8604a
DIEGOACL07 <105613359+DIEGOACL07@users.noreply.github.com>
2024-04-24 19:44:17 -0500

    correct in CalculatorScreem

--- src/config/app-theme.ts
+++ src/config/app-theme.ts
@@ -1,13 +1,13 @@
 import { StyleSheet } from "react-native";
 
 export const colors = {
-    darkGray = '#2D2D2D',
-    lightGray = '#9B9B9B',
-    orange = "#FF9427",
+    darkGray : '#2D2D2D',
+    lightGray : '#9B9B9B',
+    orange : "#FF9427",
 
-    textPrimary = "white",
-    textSecundary = "#666666",
-    background = "#000000",
+    textPrimary : "white",
+    textSecundary : "#666666",
+    background : "#000000",
 }
 
 export const styles = StyleSheet.create({
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { colors, styles } from '../../config/app-theme';
 import { CalculatorBotton } from '../components/CalculatorBotton';
-import { useCalculator } from '../hooks/useCalculator.1';
+import { useCalculator } from '../hooks/useCalculator';
 
 
 export const CalculatorScreen = () => {
@@ -44,27 +44,32 @@ export const CalculatorScreen = () => {
           }
         
       </View>
-    </View><View style={styles.row}>
+    </View>
+    <View style={styles.row}>
         <CalculatorBotton onPress={clean} blackText label='C' color={colors.lightGray} />
         <CalculatorBotton onPress={toggleSign} blackText label='+/-' color={colors.lightGray} />
         <CalculatorBotton onPress={deleteOperation} blackText label='del' color={colors.lightGray} />
         <CalculatorBotton onPress={divideOperation} label='÷' color={colors.orange} />
-      </View><View>
+    </View>
+    <View style={styles.row}>
         <CalculatorBotton onPress={ () =>  buildNumber('7') } label='7' />
         <CalculatorBotton onPress={ () =>  buildNumber('8') } label='8' />
         <CalculatorBotton onPress={ () =>  buildNumber('9') } label='9' />
         <CalculatorBotton onPress={multiplyOperation} label='x' color={colors.orange} />
-      </View><View>
+    </View>
+    <View style={styles.row}>
         <CalculatorBotton onPress={ () =>  buildNumber('4') } label='4' />
         <CalculatorBotton onPress={ () =>  buildNumber('5') } label='5' />
         <CalculatorBotton onPress={ () =>  buildNumber('6') } label='6' />
         <CalculatorBotton onPress={subtractOperation} label='-' color={colors.orange} />
-      </View><View>
+      </View>
+      <View style={styles.row}>
         <CalculatorBotton onPress={ () =>  buildNumber('1')} label='1' />
         <CalculatorBotton onPress={ () =>  buildNumber('2')} label='2' />
         <CalculatorBotton onPress={ () =>  buildNumber('3')} label='3' />
         <CalculatorBotton onPress={addOperation} label='+' color={colors.orange} />
-      </View><View>
+      </View>
+      <View style={styles.row}>
         <CalculatorBotton onPress={ () => buildNumber('0')} label='0' doubleSize />
         <CalculatorBotton onPress={ () => buildNumber('.')} label='.' />
         <CalculatorBotton onPress={calculateResult} label='=' color={colors.orange} />
